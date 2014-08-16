852ce690fd32c4678626ec750be1066293f9a03e
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-08-16 19:17:13 +1000

    Initial commit

--- spring-button-demo/ViewController.swift
+++ spring-button-demo/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
                             
+  @IBOutlet weak var button: UIButton!
+  var bubbleSound: SystemSoundID!
+  @IBOutlet weak var dampingLabel: UILabel!
+  @IBOutlet weak var dampingStepper: UIStepper!
+
+  @IBOutlet weak var velocityLabel: UILabel!
+  @IBOutlet weak var velocityStepper: UIStepper!
+
+  @IBOutlet weak var durationLabel: UILabel!
+  @IBOutlet weak var durationStepper: UIStepper!
+
+  let defaultDuration = 2.0
+  let defaultDamping = 0.25
+  let defaultVelocity = 1.9
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    dampingLabel.text = ""
+
+    durationStepper.value = defaultDuration
+    onDurationChanged(durationStepper)
+
+    dampingStepper.value = defaultDamping
+    onDampingChanged(dampingStepper)
+
+    velocityStepper.value = defaultVelocity
+    onVelocityChanged(velocityStepper)
+
+    bubbleSound = createBubbleSound()
+
+    showButtonTapped(button)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func showButtonTapped(sender: AnyObject) {
+    button.transform = CGAffineTransformMakeScale(0.1, 0.1)
+    AudioServicesPlaySystemSound(bubbleSound)
+
+    UIView.animateWithDuration(durationStepper.value,
+      delay: 0,
+      usingSpringWithDamping: CGFloat(dampingStepper.value),
+      initialSpringVelocity: CGFloat(velocityStepper.value),
+      options: UIViewAnimationOptions.AllowUserInteraction,
+      animations: {
+        self.button.transform = CGAffineTransformMakeScale(1, 1)
+      }, completion: nil)
   }
 
+  func createBubbleSound() -> SystemSoundID {
+    var soundID: SystemSoundID = 0
+    let soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "bubble", "mp3", nil)
+    AudioServicesCreateSystemSoundID(soundURL, &soundID)
+    return soundID
+  }
 
+  @IBAction func onVelocityChanged(sender: UIStepper) {
+    velocityLabel.text =  formatValue(sender.value)
+  }
+
+  @IBAction func onDampingChanged(sender: UIStepper) {
+    dampingLabel.text =  formatValue(sender.value)
+  }
+  
+  @IBAction func onDurationChanged(sender: UIStepper) {
+    durationLabel.text =  formatValue(sender.value)
+  }
+
+  func formatValue(value: Double) -> String {
+    return String(format: "%.2f", value)
+  }
 }
 
